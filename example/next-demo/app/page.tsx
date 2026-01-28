79f7df792d8fda24badb847330f1fae1d9a22013
maruf-me <marufme.dev@gmail.com>
2026-01-28 15:51:30 +0600

    feat: update some file

--- example/next-demo/app/page.tsx
+++ example/next-demo/app/page.tsx
@@ -3,21 +3,23 @@
 import { useState } from "react";
 import AllExamples, { BasicExample } from "../components/basic-usage";
 import AdvancedEditor from "../../../src";
+import { MORE_ADVANCE } from "../../../src/variants";
 
 export default function Home() {
   const [content, setContent] = useState("");
   console.log({ content });
   return (
-    <div className="bg-slate-100 min-h-screen p-8">
+    <div className="bg-slate-100 min-h-screen p-8 text-black">
       <AdvancedEditor
         height="400px"
         placeholder="Start typing..."
         defaultValue={content ?? ""}
         onChange={setContent}
-        variant="full"
+        variant="custom"
+        buttonList={MORE_ADVANCE}
       />
       {/* <BasicExample /> */}
-      {/* <AllExamples /> */}
+      <AllExamples />
     </div>
   );
 }
